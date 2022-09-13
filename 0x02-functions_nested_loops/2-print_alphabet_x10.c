bd7f0359f1ae221058787965bbe0e2ce6767e317
Adeola <adeolapraise02@gmail.com>
2022-09-13 17:02:03 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet in lowercase ten times.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabets;
+	int no_of_times;
+
+	while (no_of_times < 11)
+	{
+		for (alphabets = 97; alphabets < 123; alphabets++)
+		{
+			_putchar(alphabets);
+		}
+	}
+}
