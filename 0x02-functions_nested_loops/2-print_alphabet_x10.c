42ef2150bb3707a1835dd2533bed53546412103b
Adeola <adeolapraise02@gmail.com>
2022-09-13 17:23:49 +0000

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,12 @@ void print_alphabet_x10(void)
 	int alphabets;
 	int no_of_times;
 
-	while (no_of_times < 11)
+	for (no_of_times = 0; no_of_times <= 9; no_of_times++)
 	{
 		for (alphabets = 97; alphabets < 123; alphabets++)
 		{
 			_putchar(alphabets);
 		}
-
-		no_of_times++;
-		_putchar('\n');
+		putchar('\n');
 	}
 }
