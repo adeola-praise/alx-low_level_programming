cde1021fab0d592405f8ce6ca27c4ddfd69bbf41
Adeola <adeolapraise02@gmail.com>
2022-09-13 16:30:13 +0000

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,10 @@ int main(void)
 {
 	char string[] = "_putchar";
 	int i;
-	
-	for(i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(string[i]);
-	}	
+	}
 	_putchar('\n');
 
 	return (0);
