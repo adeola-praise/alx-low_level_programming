10182890b5dce82335d4b871e3e99c6309a7ad2a
Adeola <adeolapraise02@gmail.com>
2022-09-15 13:19:21 +0000

    Task 5 Correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,19 @@
 
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
 	int j;
 
-	while (i <= 10)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
+			if (j > 9)
+				_putchar((i / 10) + '0');
+
 			_putchar((j % 10) + '0');
 		}
+
 		_putchar('\n');
-		i++;
 	}
-	_putchar('\n');
 }
