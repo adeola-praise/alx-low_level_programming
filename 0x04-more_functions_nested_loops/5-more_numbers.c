975712fe89008e1f8b7fc1aba5abeafef158a8b6
Adeola <adeolapraise02@gmail.com>
2022-09-15 13:28:52 +0000

    Task 5 Error Correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,25 +3,21 @@
 /**
  * more_numbers - Prints 10 times the numbers, from 0 to 14,
  * followed by a new line.
- *
- * Return: void
  */
 
 void more_numbers(void)
 {
 	int i;
-	int j;
+	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (c = 0; c <= 14; c++)
 		{
-			if (j > 9)
-				_putchar((i / 10) + '0');
-
-			_putchar((j % 10) + '0');
+			if (c > 9)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
 }
