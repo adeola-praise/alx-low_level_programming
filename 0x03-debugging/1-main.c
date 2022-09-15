3ff0948ba1002dad6e9fad85cc6e439471e646b3
Adeola <adeolapraise02@gmail.com>
2022-09-15 22:23:26 +0000

    Fix Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,9 +9,14 @@ int main(void)
 {
 	int i;
 
+	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
-	printf("Infinite loop incoming :(\n");
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
