bf31a1f34194761d10ab73d6181e00828a30dfb7
Adeola <adeolapraise02@gmail.com>
2022-09-19 20:02:05 +0000

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,15 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (i >= 0)
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-
-	_putchar('\n');
 }
