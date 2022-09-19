c6f1105ed6c981a2c318a9ce7faf45d8a4fb90f7
Adeola <adeolapraise02@gmail.com>
2022-09-19 18:46:08 +0000

    Correct Error

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = i - 1; i >=0; i--)
+		for (i = i - 1; i >= 0; i--)
 		{
 			_putchar(s[i]);
 		}
