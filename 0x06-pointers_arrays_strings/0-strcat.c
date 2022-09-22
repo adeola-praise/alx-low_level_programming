66ab051f277ffe6765f106bf8dda8355948d5a0a
Adeola <adeolapraise02@gmail.com>
2022-09-22 19:13:12 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ *
+ * @dest: the destination string.
+ * @src: the source string.
+ *
+ * Return: the contenated string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		dest++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+}
