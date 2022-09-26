21c30af4bbb2ece890c82228521e13810f29a496
Adeola <adeolapraise02@gmail.com>
2022-09-26 22:00:34 +0000

    Correct Betty Error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,16 +4,17 @@
  * _memset - fills the first n bytes of the memory area,
  * pointed to by s with the constant byte b.
  *
- * @s: the pointer
- * @b: the character
- * @n: the number of bytes
+ * @s: the pointer.
+ * @b: the character.
+ * @n: the number of bytes.
  *
- * Return: a pointer to the memory area s
+ * Return: a pointer to the memory area s.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i = 0;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
