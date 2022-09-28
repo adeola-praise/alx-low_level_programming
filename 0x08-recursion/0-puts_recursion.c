5338b93bbffe81859a32188f24ab60a2cb5fcd43
Adeola <adeolapraise02@gmail.com>
2022-09-28 18:03:34 +0000

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,18 +4,20 @@
  * _puts_recursion - prints a string, followed by a new line.
  *
  * @s: pointer to the string
+ *
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s != '/0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar('/n');
+		_putchar('\n');
 		return;
 	}
 }
