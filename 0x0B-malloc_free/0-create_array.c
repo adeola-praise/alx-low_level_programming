c944dca1c509b14dce69ad44d691c677e750d417
Adeola <adeolapraise02@gmail.com>
2022-10-03 21:45:05 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,10 +21,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	if (p == 0)
+	p = malloc(sizeof(char) * size);
+
+	if (p == NULL)
 		return (NULL);
 
-	p = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
