348c41cfcb473996d9011f4a3c1aaa8765652d0c
Adeola <adeolapraise02@gmail.com>
2022-10-21 23:08:00 +0000

    Correct Betty Error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,9 +18,9 @@ size_t print_list(const list_t *h)
 	{
 		count++;
 		if (h->str == NULL)
-			printf ("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf ("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
 	}
